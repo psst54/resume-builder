2abe1db7bef7108fbad60a1850727d40705ee5cd
psst54 <psst5491@naver.com>
2023-07-30 23:29:42 +0900

    🐛 fix error with app route

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: "export",
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
