37809df30830343f608b9c804581480a8e126539
psst54 <psst5491@naver.com>
2023-07-30 23:41:17 +0900

    🐛 use edge runtime

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: "export",
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
