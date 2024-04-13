6182174e492a8a63ccf04ee6b7bf9453b01a112b
psst54 <psst5491@naver.com>
2024-04-13 21:38:28 +0900

    🎨 공통 스타일 코드 묶기

--- src/app/build/styles.ts
+++ src/app/build/styles.ts
@@ -1,5 +1,6 @@
-import styled from "@emotion/styled";
 import { COLOR } from "@/styles/color";
+
+const mq = "@media (max-width: 800px)";
 
 export const container = {
   minHeight: "100vh",
@@ -13,13 +14,15 @@ export const body = {
   display: "flex",
 };
 
-export const inputArea = {
+const halfScreen = {
   flexShrink: 0,
 
   width: "50%",
   height: "calc(100vh - 4rem)",
-  overflowY: "scroll",
+  overflowY: "scroll" as const,
+};
 
+const scrollBar = {
   "::-webkit-scrollbar": {
     background: "transparent",
     width: "0.8rem",
@@ -30,46 +33,41 @@ export const inputArea = {
     width: "0.8rem",
     borderRadius: "0.2rem",
   },
+};
 
-  "@media (max-width: 800px)": {
+const thinScrollBar = {
+  "::-webkit-scrollbar": {
+    background: "transparent",
+    width: "0.6rem",
+  },
+
+  "::-webkit-scrollbar-thumb": {
+    background: COLOR.GRAY.STANDARD,
+    width: "0.6rem",
+    borderRadius: "0.2rem",
+  },
+};
+
+export const inputArea = {
+  ...halfScreen,
+  ...scrollBar,
+
+  [mq]: {
     width: "100%",
-
-    "::-webkit-scrollbar": {
-      background: "transparent",
-      width: "0.6rem",
-    },
-
-    "::-webkit-scrollbar-thumb": {
-      background: COLOR.GRAY.STANDARD,
-      width: "0.6rem",
-      borderRadius: "0.2rem",
-    },
+    ...thinScrollBar,
   },
 };
 
 export const previewArea = {
-  flexShrink: 0,
-
-  width: "50%",
-  height: "calc(100vh - 4rem)",
-  overflowY: "scroll",
+  ...halfScreen,
+  ...scrollBar,
 
   display: "flex",
-
   background: COLOR.LIGHT_GRAY.STANDARD,
 
-  "::-webkit-scrollbar": {
-    background: "transparent",
-    width: "0.8rem",
-  },
+  ...scrollBar,
 
-  "::-webkit-scrollbar-thumb": {
-    background: COLOR.GRAY.STANDARD,
-    width: "0.8rem",
-    borderRadius: "0.2rem",
-  },
-
-  "@media (max-width: 800px)": {
+  [mq]: {
     width: 0,
   },
 };
