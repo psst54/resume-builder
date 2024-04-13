93e360b77b5fed2160ea38d9027b1366dfd40ac9
psst54 <psst5491@naver.com>
2024-04-13 21:16:40 +0900

    🐛 useSearchParams에 suspense 추가

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -2,6 +2,7 @@
 
 "use client";
 
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 const container = {
@@ -15,13 +16,20 @@ const container = {
   minHeight: "100dvh",
 };
 
-export default function ErrorPage() {
+function ErrorMessage() {
   const message = useSearchParams().get("message");
 
+  if (!message) return null;
+  return <p>오류 메세지 : {message}</p>;
+}
+
+export default function ErrorPage() {
   return (
     <div css={container}>
       <p>오류가 발생했습니다.</p>
-      {message && <p>오류 메세지 : {message}</p>}
+      <Suspense>
+        <ErrorMessage />
+      </Suspense>
       <a href="/">메인 페이지로 돌아가기</a>
     </div>
   );
